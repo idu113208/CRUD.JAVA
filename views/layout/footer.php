7a11f2c156975a94ace59955978fd762cf5e9f44
YOHANES_KA <idu113208@gmail.com>
2025-12-16 12:14:05 +0700

    Revert "Create Alumni Information System with Tracer Study and GIS"

--- views/layout/footer.php
+++
@@ -1,8 +0,0 @@
-        <div class="footer">
-            <p>&copy; <?php echo date('Y'); ?> Sistem Informasi Alumni. All Rights Reserved.</p>
-        </div>
-    </div>
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
